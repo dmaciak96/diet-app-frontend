37cd48f470110571fe99438a796cac4b47db1c83
Dawid <maciak.dawid.96@gmail.com>
2023-06-12 15:04:03 +0200

    init project

---
+++ src/main/java/pl/daveproject/frontendservice/WebdietFrontendServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.daveproject.frontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdietFrontendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdietFrontendServiceApplication.class, args);
+    }
+
+}
