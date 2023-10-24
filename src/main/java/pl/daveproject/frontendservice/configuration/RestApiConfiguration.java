0cd4421c0b6cbc2ef9301873197e41efb7a970ca
Dawid <maciak.dawid.96@gmail.com>
2023-10-24 14:13:13 +0200

    Remove keycloak dependency from project

--- src/main/java/pl/daveproject/frontendservice/configuration/RestApiConfiguration.java
+++ src/main/java/pl/daveproject/frontendservice/configuration/RestApiConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -15,11 +21,31 @@ public class RestApiConfiguration {
     private String backendServiceUrl;
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        var oauth2Client =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+
         return WebClient.builder()
                 .uriBuilderFactory(new DefaultUriBuilderFactory(backendServiceUrl))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .apply(oauth2Client.oauth2Configuration())
                 .build();
+    }
+
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        var authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .refreshToken()
+                .build();
+        var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
     }
 }
--- src/main/java/pl/daveproject/frontendservice/configuration/security/SecurityConfiguration.java
+++ src/main/java/pl/daveproject/frontendservice/configuration/security/SecurityConfiguration.java
@@ -1,39 +1,25 @@
 package pl.daveproject.frontendservice.configuration.security;
 
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration extends VaadinWebSecurity {
 
-    private final CustomLogoutHandler customLogoutHandler;
-    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+    @Value("${authorization-endpoint}")
+    private String authorizationEndpoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/images/*.png", "/", "/login**", "/callback/", "/webjars/**",
-                                "/error**").permitAll())
-                .oauth2Login(login -> login.defaultSuccessUrl("/dashboard"))
-                .oauth2Client(Customizer.withDefaults())
-                .oauth2ResourceServer(auth2 -> auth2.jwt(
-                        token -> token.jwtAuthenticationConverter(jwtAuthenticationConverter)))
-                .logout(logout -> logout.addLogoutHandler(customLogoutHandler)
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID"));
+        http.oauth2Login(oauth2Login ->
+                        oauth2Login.loginPage(authorizationEndpoint))
+                .oauth2Client(Customizer.withDefaults());
         super.configure(http);
     }
 }
