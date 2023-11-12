8eff261d54d5ce9a6fc94bd3ed6ec34d9efe728f
Dawid <maciak.dawid.96@gmail.com>
2023-11-12 21:34:55 +0100

    setup logout

--- src/main/java/pl/daveproject/frontendservice/configuration/security/CustomLogoutHandler.java
+++ src/main/java/pl/daveproject/frontendservice/configuration/security/CustomLogoutHandler.java
@@ -4,11 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
@@ -17,16 +18,16 @@ public class CustomLogoutHandler implements LogoutHandler {
 
     private static final String AUTH_SERVER_LOGOUT_ENDPOINT = "/logout";
 
-    private final WebClient webClient;
+    @Value("${spring.security.oauth2.client.provider.spring.issuer-uri}")
+    private String authServerBaseUri;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response,
                        Authentication authentication) {
-        var builder = UriComponentsBuilder.fromUriString(AUTH_SERVER_LOGOUT_ENDPOINT);
-        webClient.post()
-                .uri(builder.toUriString())
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        var restTemplate = new RestTemplate();
+        var authServerResponse = restTemplate.getForEntity(authServerBaseUri + AUTH_SERVER_LOGOUT_ENDPOINT, Void.class);
+        if (authServerResponse.getStatusCode() == HttpStatus.OK) {
+            log.info("Successfully logout from the system {}", authentication.getName());
+        }
     }
 }
--- src/main/java/pl/daveproject/frontendservice/configuration/security/SecurityConfiguration.java
+++ src/main/java/pl/daveproject/frontendservice/configuration/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -23,7 +24,9 @@ public class SecurityConfiguration extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login(oauth2Login ->
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(oauth2Login ->
                         oauth2Login.loginPage(authorizationEndpoint)
                                 .defaultSuccessUrl(LOGIN_SUCCESS_URL))
                 .logout(logout -> logout.addLogoutHandler(customLogoutHandler)
